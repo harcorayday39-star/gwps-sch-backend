20d4a46cb7cd8c273801267cbee347ce196f294c
harcorayday39-star <harcorayday39@gmail.com>
2025-09-10 01:51:36 -0700

    Delete models directory

--- models/Config.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const configSchema = new Schema({
-  key: { type: String, unique: true },
-  value: Schema.Types.Mixed
-});
-module.exports = mongoose.model('Config', configSchema);
